220d5ad43334972602c1d645023a3a6f89683172
dgadiraju <dgadiraju@itversity.com>
2023-06-07 18:32:32 +0530

    Modularized courses routes

--- app.py
+++ app.py
@@ -16,9 +16,8 @@ password = os.environ.get('SALES_DB_PASS')
 app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user}:{password}@{host}:{port}/{db_name}'
 db.init_app(app)
 
-from routes import users
+from routes import users, courses
 from models.user import User
-from models.course import Course
 
 
 @app.route('/')
@@ -26,11 +25,3 @@ def hello_world():
     # rec = db.get_or_404(User, 1)
     rec = User.query.get_or_404(1)
     return render_template('index.html', user=rec)
-
-
-@app.route('/courses')
-def courses():
-    course_recs = db.session.query(Course).all()
-    courses = list(map(lambda rec: rec.__dict__, course_recs))
-    return render_template('courses.html', courses=courses)
-
---
+++ routes/courses.py
@@ -0,0 +1,12 @@
+from flask import render_template
+
+from app import app
+from app import db
+from models.course import Course
+
+
+@app.route('/courses')
+def courses():
+    course_recs = db.session.query(Course).all()
+    courses = list(map(lambda rec: rec.__dict__, course_recs))
+    return render_template('courses.html', courses=courses)
